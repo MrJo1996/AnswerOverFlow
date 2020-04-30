6d6b638ed943110a9de6647b4cd6adc82319ce91
marianobuttino <mariano.butt@gmail.com>
2020-04-30 15:42:16 +0200

    ..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -43,63 +42,14 @@ const routes: Routes = [
   {
     path: 'recupera-password',
     loadChildren: () => import('./recupera-password/recupera-password.module').then( m => m.RecuperaPasswordPageModule)
-  },  {
+  },
+  {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-=======
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
-    path: 'prova',
-    loadChildren: () => import('./prova/prova.module').then( m => m.PROVAPageModule)
-  },
-  {
-    path: 'modifica-sondaggio',
-    loadChildren: () => import('./modifica-sondaggio/modifica-sondaggio.module').then( m => m.ModificaSondaggioPageModule)
-  },
-  {
-    path: 'modifica-risposta',
-    loadChildren: () => import('./modifica-risposta/modifica-risposta.module').then( m => m.ModificaRispostaPageModule)
-  },
-  {
-    path: 'modifica-profilo',
-    loadChildren: () => import('./modifica-profilo/modifica-profilo.module').then( m => m.ModificaProfiloPageModule)
-  },
-  {
-    path: 'modifica-domanda',
-    loadChildren: () => import('./modifica-domanda/modifica-domanda.module').then( m => m.ModificaDomandaPageModule)
-  },
-  {
-    path: 'segnala-utente',
-    loadChildren: () => import('./segnala-utente/segnala-utente.module').then( m => m.SegnalaUtentePageModule)
-  },
-  {
-    path: 'inserisci-domanda',
-    loadChildren: () => import('./inserisci-domanda/inserisci-domanda.module').then( m => m.InserisciDomandaPageModule)
-  },
-  {
-    path: 'recupera-password',
-    loadChildren: () => import('./recupera-password/recupera-password.module').then( m => m.RecuperaPasswordPageModule)
   },
   {
     path: 'proponi-categoria',
     loadChildren: () => import('./proponi-categoria/proponi-categoria.module').then( m => m.ProponiCategoriaPageModule)
->>>>>>> 359d29873570c4f12ef2495d92d01ae4bc4990bd
-  },
-  {
-    path: 'viualizza-profilo',
-    loadChildren: () => import('./viualizza-profilo/viualizza-profilo.module').then( m => m.ViualizzaProfiloPageModule)
   },
   {
     path: 'visualizza-profilo',
