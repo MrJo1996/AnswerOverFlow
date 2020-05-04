edf17a2496d1c8e82fe2a7e891149459e0d15e58
marianobuttino <mariano.butt@gmail.com>
2020-05-04 23:06:57 +0200

    Modifica del file "modifica-sondaggio.html"

--- src/app/modifica-sondaggio/modifica-sondaggio.page.ts
+++ src/app/modifica-sondaggio/modifica-sondaggio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-modifica-sondaggio',
@@ -7,7 +8,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModificaSondaggioPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
+
+  async popupModificaTitolo() {
+    const alert = await this.alertController.create({
+      header: 'Modifica',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Titolo'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
+  }
+
+  async popupModificaTimer() {
+    const alert = await this.alertController.create({
+      header: 'Modifica',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'time',
+          placeholder: 'Timer'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
+  }
+
+  async popupConfermaEliminaSondaggio() {
+    const alert = await this.alertController.create({
+      header: 'Elimina sondaggio',
+      message: 'Sicuro di voler eliminare questo sondaggio?',
+      buttons: ['Conferma']
+    });
+
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
+  }
 
   ngOnInit() {
   }
