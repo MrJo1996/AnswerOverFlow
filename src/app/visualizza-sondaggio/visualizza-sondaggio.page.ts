6f427263b9d44bed1518a5f8137fbce83a2ced8e
simtape <s.cassetta1@studenti.unimol.it>
2020-05-06 22:33:10 +0200

    modifiche sul ts di visualizza-sondaggio

--- src/app/visualizza-sondaggio/visualizza-sondaggio.page.ts
+++ src/app/visualizza-sondaggio/visualizza-sondaggio.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-visualizza-sondaggio',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visualizza-sondaggio.page.scss'],
 })
 export class VisualizzaSondaggioPage implements OnInit {
+  
+  titolo = "Come ti chiami?"
+  data_e_ora = "18-09-2019"
+  categoria = "curiosita"
+  descrizione = "voglio sapere il tuo nome"
+  scelte = [{
+                  num_favorevoli: 1,
+                  descrizione: 'michele'
+                },
+                {
+                  num_favorevoli: 1,
+                  descrizione: 'giovanni'
+                },
+                {
+                  num_favorevoli: 1,
+                  descrizione: 'giuseppe'
+                },
+                {
+                  num_favorevoli: 1,
+                  descrizione: 'fausto'
+                }] 
 
+  voti_totali = this.scelte[0].num_favorevoli + 
+                this.scelte[1].num_favorevoli + 
+                this.scelte[2].num_favorevoli + 
+                this.scelte[3].num_favorevoli 
+
+  
   constructor() { }
 
   ngOnInit() {
   }
+  
+  @ViewChild('content', {read:IonContent,static: false}) myContent: IonContent;
+
 
 }
