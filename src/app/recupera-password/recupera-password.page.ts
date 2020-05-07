c9c2795338af999d5f06115b6fabd7f73ab597c3
frankiafi <francesco.iafi@gmail.com>
2020-05-07 10:50:09 +0200

    'recupera-password': creazione metodo 'postInvio'

--- src/app/recupera-password/recupera-password.page.ts
+++ src/app/recupera-password/recupera-password.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Promise } from "q";
+
+import { PostServiceService } from "../Services/post-service.service";
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
 @Component({
   selector: 'app-recupera-password',
   templateUrl: './recupera-password.page.html',
   styleUrls: ['./recupera-password.page.scss'],
 })
 export class RecuperaPasswordPage implements OnInit {
+  
+  email: '';
+  request: Promise<any>;
+  result: Promise<any>;
+  url = 'http://answeroverflow.altervista.org/AnswerOverFlow-BackEnd/public/index.php/recupero'
 
-  constructor() { }
+  constructor(private service: PostServiceService, private router: Router, private navctrl: NavController) { }
 
   ngOnInit() {
   }
 
+  postInvio(){
+    let postData ={
+      "email": this.email 
+    };
+    this.result = this.service.postService(postData, this.url).then((data) => {
+      this.request = data;
+      console.log(data);
+    }, err => {
+      console.log(err.message);
+    });
+  }
 }
