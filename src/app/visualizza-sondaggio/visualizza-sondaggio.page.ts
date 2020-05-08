795d0121ea215fcbf2cae6460da72e451dbd5394
simtape <s.cassetta1@studenti.unimol.it>
2020-05-08 16:38:26 +0200

    elimina sondaggio

--- src/app/visualizza-sondaggio/visualizza-sondaggio.page.ts
+++ src/app/visualizza-sondaggio/visualizza-sondaggio.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonContent } from '@ionic/angular';
+import { Promise } from "q";
+import { PostServiceService } from "../Services/post-service.service";
+
+
 
 @Component({
   selector: 'app-visualizza-sondaggio',
@@ -12,6 +16,7 @@ export class VisualizzaSondaggioPage implements OnInit {
   data_e_ora = "18-09-2019"
   categoria = "curiosita"
   descrizione = "voglio sapere il tuo nome"
+  codice_sondaggio = 4
   scelte = [{
                   num_favorevoli: 1,
                   descrizione: 'michele'
@@ -34,13 +39,36 @@ export class VisualizzaSondaggioPage implements OnInit {
                 this.scelte[2].num_favorevoli + 
                 this.scelte[3].num_favorevoli 
 
+
+  request: Promise<any>;
+  result: Promise<any>;
+
+  url = 'http://answeroverflow.altervista.org/AnswerOverFlow-BackEnd/public/index.php/cancellaSondaggio/4'
+
   
-  constructor() { }
+  constructor(private service: PostServiceService) { }
 
   ngOnInit() {
   }
   
   @ViewChild('content', {read:IonContent,static: false}) myContent: IonContent;
 
+elimina(){
 
+  let deleteData ={
+      "codice_sondaggio" : this.codice_sondaggio
+
+  }
+
+this.result = this.service.deleteService(this.url, deleteData ).then((data) =>{
+  this.request = data;
+  console.log(data);
+}, err => {
+  console.log(err.message);
+});
 }
+}
+
+
+
+
