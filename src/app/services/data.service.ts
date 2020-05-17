b363fa6c9b55fb3a7c431f290fc15c3d24ff2958
Chris <c.peluso1@studenti.unimol.it>
2020-05-17 23:46:06 +0200

    Aggiunte un paio di funzionalità per i dati da dover comunicare

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -13,7 +13,9 @@ export class DataService {
   emailUtente;
   emailOthers;
   codice_chat;
+
   constructor(private http: HttpClient) { }
+
   setUtente(id:string, username:string,password:string, nome:string,cognome:string,bio:string){
     this.utente['0']=id;
     this.utente['1']=username;
@@ -25,8 +27,6 @@ export class DataService {
   setCod_domanda(id_domanda:number){
     this.codice_domanda = id_domanda;
   }
-  setEmail_utente(id_utente:string){
-    this.email = id_utente;}
   setCod_sondaggio(id_sondaggio:number){
     this.codice_sondaggio = id_sondaggio;
   }
@@ -36,7 +36,6 @@ export class DataService {
   setCodice_chat(cod_chat: number) {
     this.codice_chat = cod_chat;
   }
- 
   setEmailOthers(emailOthers) {
     this.emailOthers = emailOthers;
   }
@@ -53,7 +52,6 @@ export class DataService {
   getCodice_chat() {
     return this.codice_chat;
   }
- 
   getEmailOthers(emailOthers) {
     return this.emailOthers;
   }
