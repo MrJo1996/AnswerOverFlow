b00c2e63750b7e3d27f08f880a5033e6c8ab13d0
Chris <c.peluso1@studenti.unimol.it>
2020-06-07 12:04:59 +0200

    Ora si può distinguere un ospite da un utente

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,55 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 import { Router } from "@angular/router";
 
 import { Promise } from "q";
-import { Storage } from '@ionic/storage';
+import { Storage } from "@ionic/storage";
 import { PostServiceService } from "../services/post-service.service";
 import { NavController } from "@ionic/angular";
 import { DataService } from "../services/data.service";
-import { ToastController } from '@ionic/angular';
-import { ApiService } from 'src/app/providers/api.service';
+import { ToastController } from "@ionic/angular";
+import { ApiService } from "src/app/providers/api.service";
 import { MenuController } from "@ionic/angular";
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
+import { AppComponent } from "../app.component";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"],
 })
 export class LoginPage implements OnInit {
-
   click = false;
-  email = '';
+  email = "";
   username = "";
   password = "";
-  nome = '';
-  cognome = '';
-  bio = '';
+  nome = "";
+  cognome = "";
+  bio = "";
 
   request: Promise<any>;
   result: Promise<any>;
-  url = 'http://answeroverflow.altervista.org/AnswerOverFlow-BackEnd/public/index.php/login'
+  url =
+    "http://answeroverflow.altervista.org/AnswerOverFlow-BackEnd/public/index.php/login";
 
-  constructor(public apiService: ApiService,
+  constructor(
+    public apiService: ApiService,
     public toastController: ToastController,
     private dataService: DataService,
     private service: PostServiceService,
     private router: Router,
     private navctrl: NavController,
     private storage: Storage,
-    private menuCtrl: MenuController
-  ) { }
+    private menuCtrl: MenuController,
+    private menuSet: AppComponent
+  ) {}
 
   ngOnInit() {
     //disable scroll (anche su ios)
-    var fixed = document.getElementById('fixed');
+    var fixed = document.getElementById("fixed");
 
-    fixed.addEventListener('touchmove', function (e) {
-
-      e.preventDefault();
-
-    }, false);
+    fixed.addEventListener(
+      "touchmove",
+      function (e) {
+        e.preventDefault();
+      },
+      false
+    );
   }
 
   ionViewWillEnter() {
@@ -57,139 +62,142 @@ export class LoginPage implements OnInit {
   }
 
   ionViewWillLeave() {
-    this.menuCtrl.enable(true);
+    this.storage.get("session").then((data) => {
+      console.log("SESSION:" + data);
+    });
   }
 
   public reg() {
-    this.router.navigate(['registrazione']);
+    this.router.navigate(["registrazione"]);
   }
 
+  public recupero() {
+    this.router.navigate(["recupera-password"]);
+  }
 
   postLogin() {
     if (this.password.length < 8) {
-      const toast = document.createElement('ion-toast');
-      toast.message = 'Password troppo corta o non valida!';
+      const toast = document.createElement("ion-toast");
+      toast.message = "Password troppo corta o non valida!";
       toast.duration = 2000;
       toast.position = "top";
-      toast.style.fontSize = '20px';
-      toast.color = 'danger';
-      toast.style.textAlign = 'center';
+      toast.style.fontSize = "20px";
+      toast.color = "danger";
+      toast.style.textAlign = "center";
       document.body.appendChild(toast);
       return toast.present();
     } else {
       let postData = {
-        "username": this.username,
-        "password": this.password
+        username: this.username,
+        password: this.password,
       };
 
-      this.result = this.service.postService(postData, this.url).then((data) => {
-        this.request = data;
-        console.log(data);
+      this.result = this.service.postService(postData, this.url).then(
+        (data) => {
+          this.request = data;
+          console.log(data);
 
-        this.checkField(data);
-        this.clickLogin(!data.error, data);
-
-        
-      }, err => {
-        console.log(err.message);
-
-      })
+          this.checkField(data);
+          this.clickLogin(!data.error, data);
+        },
+        (err) => {
+          console.log(err.message);
+        }
+      );
     }
   }
 
   italian_bad_words_check(input: string) {
-    let list = require('italian-badwords-list');
+    let list = require("italian-badwords-list");
     let array = list.array;
     return array.includes(input);
   }
 
   checkField(data) {
-    if ((this.italian_bad_words_check(this.username) || this.italian_bad_words_check(this.password))) {
-      const toast = document.createElement('ion-toast');
+    if (
+      this.italian_bad_words_check(this.username) ||
+      this.italian_bad_words_check(this.password)
+    ) {
+      const toast = document.createElement("ion-toast");
 
-      toast.message = 'Hai inserito una parola scorretta!';
+      toast.message = "Hai inserito una parola scorretta!";
       toast.duration = 2000;
       toast.position = "top";
-      toast.style.fontSize = '20px';
-      toast.color = 'danger';
-      toast.style.textAlign = 'center';
+      toast.style.fontSize = "20px";
+      toast.color = "danger";
+      toast.style.textAlign = "center";
 
       document.body.appendChild(toast);
       return toast.present();
-
-    }
-    else if (this.username.length < 1 || this.password.length < 8) {
-      const toast = document.createElement('ion-toast');
-      toast.message = 'Devi inserire un username valido!';
+    } else if (this.username.length < 1 || this.password.length < 8) {
+      const toast = document.createElement("ion-toast");
+      toast.message = "Devi inserire un username valido!";
       toast.duration = 2000;
       toast.position = "top";
-      toast.style.fontSize = '20px';
-      toast.color = 'danger';
-      toast.style.textAlign = 'center';
+      toast.style.fontSize = "20px";
+      toast.color = "danger";
+      toast.style.textAlign = "center";
       document.body.appendChild(toast);
       return toast.present();
-
-
-    } else if (data.error == (true)) {
-
-      const toast = document.createElement('ion-toast');
-      toast.message = 'Credenziali errate!';
+    } else if (data.error == true) {
+      const toast = document.createElement("ion-toast");
+      toast.message = "Credenziali errate!";
       toast.duration = 2000;
       toast.position = "top";
-      toast.style.fontSize = '20px';
-      toast.color = 'danger';
-      toast.style.textAlign = 'center';
+      toast.style.fontSize = "20px";
+      toast.color = "danger";
+      toast.style.textAlign = "center";
       document.body.appendChild(toast);
       return toast.present();
-
-
     }
   }
 
   clickLogin(condizione, data) {
-
     if (condizione) {
       //TODO SETTARE NOME E COGNOME COME USERNAME
-      this.dataService.setUsername(data.data[0]['username']); 
-      this.dataService.setNome(data.data[0]['nome']); 
-      this.dataService.setCognome(data.data[0]['cognome']); 
+      this.dataService.setUsername(data.data[0]["username"]);
+      this.dataService.setNome(data.data[0]["nome"]);
+      this.dataService.setCognome(data.data[0]["cognome"]);
 
-      this.storage.set('utente', data.data[0]);
-      this.storage.set('session', true);
-      console.log('false', data);
+      this.storage.set("utente", data.data[0]);
+      this.storage.set("session", true);
+      console.log("false", data);
       this.click = true;
 
-      this.storage.set('session', true);
+      this.storage.set("session", true);
 
       setTimeout(() => {
-        this.storage.get('session').then(data => {
-          console.log('SESSION:' + data)
+        this.storage.get("session").then((data) => {
+          console.log("SESSION:" + data);
         });
 
-        this.storage.get('utente').then(data => {
+        this.storage.get("utente").then((data) => {
           this.dataService.emailUtente = data.email;
         });
+      }, 1000);
 
-      }, 3000);
-
-      this.storage.get('session').then(data => {
-        this.storage.set('session', true);
-        console.log('SESSION:' + data)
-      });
-      this.router.navigate(['home']);
+      // this.storage.get('session').then(data => {
+      //   this.storage.set('session', true);
+      //   console.log('SESSION:' + data)
+      // });
+      this.router.navigate(["home"]);
     } else {
-      console.log('error');
+      console.log("error");
 
       /* this.dataService.setUtente(this.email, this.username, this.password, this.nome, this.cognome,this.bio);
        this.router.navigate(['home']);
        console.log(this.dataService.utente);*/
-
     }
-
   }
 
+  clickOspite() {
+    //TODO SETTARE NOME E COGNOME COME USERNAME
+    this.dataService.setUsername("");
+    this.dataService.setNome("Ospite");
+    this.dataService.setCognome("");
 
+    this.storage.set("utente", false);
+    this.storage.set("session", false);
+    this.router.navigate(["home"]);
+  }
 }
-
-
-
