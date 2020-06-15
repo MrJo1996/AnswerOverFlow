1ade4074de8d7e402f574e4304d256b01ac23e9f
frankiafi <francesco.iafi@gmail.com>
2020-06-15 20:24:57 +0200

    aggiunta commenti

--- src/app/proponi-categoria/proponi-categoria.page.ts
+++ src/app/proponi-categoria/proponi-categoria.page.ts
@@ -10,23 +10,24 @@ import { Storage } from "@ionic/storage";
   templateUrl: './proponi-categoria.page.html',
   styleUrls: ['./proponi-categoria.page.scss'],
 })
-export class ProponiCategoriaPage implements OnInit {
+export class ProponiCategoriaPage implements OnInit{
   proposta =  '';
-  cod_utente;
+  cod_utente = '';
   request: Promise<any>;
   result: Promise<any>;
 
-  constructor(private menuCtrl: MenuController, public apiService: ApiService, private storage: Storage, public alertController: AlertController, private router: Router, private navCtrl: NavController, private dataService: DataService) { }
+  constructor(private menuCtrl: MenuController, public apiService: ApiService, private storage: Storage, public alertController: AlertController, private router: Router, private navCtrl: NavController, private dataService: DataService) {}
 
-  ngOnInit() {
+  ngOnInit(){
+    //Richiamo dei dati dell'utente loggato dallo storage
     this.storage.get('utente').then(
       (data)=>{
         this.cod_utente = data.email
       });
     }
 
-  italian_bad_words_check(input: string){
-    var check;
+  italian_bad_words_check(input: string){//controllo parole scorrette dal dizionario italiano
+    var check: boolean;
     
     let list = require('italian-badwords-list');
     let array = list.array;
@@ -42,14 +43,14 @@ export class ProponiCategoriaPage implements OnInit {
     return check;
   }
 
-  english_bad_words_check(input: string){
+  english_bad_words_check(input: string){//controllo parole scorrette dal dizionario inglese
     var Filter = require('bad-words'),
     filter = new Filter();
     
     return filter.isProfane(input);
   }
 
-  bad_words_toast(){
+  bad_words_toast(){//Alert di errore per inserimento parola scorretta
     const toast = document.createElement('ion-toast');
     toast.message = 'Hai inserito una o più parole non consentite!';
     toast.duration = 2000;
@@ -62,7 +63,7 @@ export class ProponiCategoriaPage implements OnInit {
     return toast.present();    
   }
 
-  max_lenght_exceeded_toast(){
+  max_lenght_exceeded_toast(){//Alert di errore per inserimento proposta troppo lunga
     const toast = document.createElement('ion-toast');
     toast.message = 'Il nome inserito per la nuova categoria è troppo lungo!';
     toast.duration = 2000;
@@ -75,7 +76,7 @@ export class ProponiCategoriaPage implements OnInit {
     return toast.present();
   }
 
-  post_invio(){
+  post_invio(){//Invio dei dati al backend
     if(this.proposta.length>20){
       this.max_lenght_exceeded_toast();
     } else{
@@ -88,16 +89,16 @@ export class ProponiCategoriaPage implements OnInit {
     }
   }
 
-  goToConfirm(){
+  goToConfirm(){//Reindirizzamento alla pagina di conferma dei dati inseriti e dell'invio mail
     this.dataService.setNuovaProposta(this.proposta);
     this.router.navigate(['conferma-invio-proposta']);
   }
 
-  goback(){
+  goback(){//Reindirizzamento alla pagina precedente
     this.navCtrl.back();
   }
 
-  openMenu(){
+  openMenu(){//Funzione per l'apertura del menu laterale
     this.menuCtrl.open();
   }
 }
--- src/app/recupera-password/recupera-password.page.ts
+++ src/app/recupera-password/recupera-password.page.ts
@@ -10,39 +10,36 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./recupera-password.page.scss'],
 })
 
-export class RecuperaPasswordPage implements OnInit {
+export class RecuperaPasswordPage implements OnInit{
 
   email = '';
   request: Promise<any>;
   result: Promise<any>;
   url = 'http://answeroverflow.altervista.org/AnswerOverFlow-BackEnd/public/index.php/recupero'
 
-  constructor(private service: PostServiceService, private router: Router, private menuCtrl: MenuController) { }
+  constructor(private service: PostServiceService, private router: Router, private menuCtrl: MenuController){}
 
-  ngOnInit() {
-    this.menuCtrl.enable(false);
+  ngOnInit(){
+    this.menuCtrl.enable(false);//disabilita menu laterale
     
     //disabilita scroll (anche su ios)
     var fixed = document.getElementById('fixed');
-
-    fixed.addEventListener('touchmove', function (e) {
-
+    fixed.addEventListener('touchmove', function (e){
       e.preventDefault();
-
     }, false);
   }
 
-  is_email_valid(email: string) {
-    var format: RegExp;
+  is_email_valid(input_email: string){//Funzione per il controllo del formato della mail (esempio@mail.it)
+    var email_format: RegExp;
 
-    format = /^([a-zA-z0-9_.\-/])+\@([a-zA-z0-9_.\-/])+\.([a-zA-Z]{2,4})$/;
-    if (!format.test(email)) {
+    email_format = /^([a-zA-z0-9_.\-/])+\@([a-zA-z0-9_.\-/])+\.([a-zA-Z]{2,4})$/;
+    if (!email_format.test(input_email)){
       return false;
     }
     return true;
   }
 
-  email_length_toast() {
+  email_length_exceeded_toast(){//Alert di errore per inserimento email troppo lunga
     const toast = document.createElement('ion-toast');
     toast.message = 'La mail inserita è troppo lunga!';
     toast.duration = 2000;
@@ -55,26 +52,26 @@ export class RecuperaPasswordPage implements OnInit {
     return toast.present();
   }
 
-  post_invio() {
-    if (this.email.length > 100) {
-      this.email_length_toast();
-    } else {
-      let postData = {
+  post_invio(){//Invio dei dati al backend
+    if (this.email.length > 100){
+      this.email_length_exceeded_toast();
+    } else{
+      let postData={
         "email": this.email
       };
       this.result = this.service.postService(postData, this.url).then(
-        (data) => {
+        (data)=>{
           this.request = data;
         });
       this.goToConfirm();
     }
   }
 
-  goback() {
+  goback(){//funzione di reindirizzamento alla pagina precedente
     this.router.navigate(['login']);
   }
 
-  goToConfirm() {    
+  goToConfirm(){//funzione di reindirizzamento alla pagina di conferma 
     this.router.navigate(['conferma-recupero']);
   }
 }
