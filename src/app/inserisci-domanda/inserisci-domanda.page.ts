b521d2f90078dc0a31d844a28506a95774676287
Davide <daviderusso125@gmail.com>
2020-06-15 16:51:18 +0200

    Pulizia codice 'Inserisci domanda'

--- src/app/inserisci-domanda/inserisci-domanda.page.ts
+++ src/app/inserisci-domanda/inserisci-domanda.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/providers/api.service';
-import { PostServiceService } from "../services/post-service.service";
 import { DataService } from "../services/data.service";
 import { AlertController, MenuController } from '@ionic/angular';
 import { PickerController } from "@ionic/angular";
@@ -9,6 +8,7 @@ import { Router } from "@angular/router";
 import { NavController } from "@ionic/angular";
 import { Storage } from "@ionic/storage";
 import { ToastController } from '@ionic/angular';
+import { PostServiceService } from "../services/post-service.service";
 
 @Component({
   selector: 'app-inserisci-domanda',
@@ -33,8 +33,8 @@ export class InserisciDomandaPage implements OnInit {
   categoriaView;
   categoriaSettings: any = [];
 
-  timerToPass: string;   //param per le funzioni
-  timerView;             //var per la view dei valori
+  timerToPass: string;
+  timerView;
   timerSettings: string[] = ["5 min", "15 min", "30 min", "1 ora", "3 ore", "6 ore", "12 ore", "1 giorno", "3 giorni"];
 
   constructor(public apiService: ApiService,
@@ -47,169 +47,39 @@ export class InserisciDomandaPage implements OnInit {
     private storage: Storage,
     public toastController: ToastController) { }
 
+
   ngOnInit() {
+
     this.apiService.prendiCategorie(this.urlCategorie).then(
       (categories) => {
         this.categoriaSettings = categories;
-        console.log('Categorie visualizzate con successo.', this.categoriaSettings);
       },
-      (rej) => {
-        console.log("C'è stato un errore durante la visualizzazione delle categorie.");
-      }
+      (rej) => { }
     );
-
     this.storage.get('utente').then(data => { this.cod_utente = data.email });
-      console.log(this.cod_utente);
+
   }
 
-
-  //INVIO DATI AL SERVER
-
-  async postInvio() {
-
-    this.apiService.inserisciDomanda(this.timerToPass, this.titolo, this.descrizione, this.cod_utente, this.codCategoriaScelta).then(
-      (result) => {
-
-        console.log('INSERIMENTO AVVENUTO CON SUCCESSO:', this.titolo, this.timerToPass, this.descrizione, this.cod_utente, this.cod_categoria);
-
-      },
-      (rej) => {
-        console.log('INSERIMENTO NON RIUSCITO');
-      }
-    );
-  }
-
-
-  //CHECK  ALL FIELD
 
   async checkField() {
 
-    if ((this.checkIfThereAreItalianBadWords(this.titolo) || this.checkIfThereAreEnglishBadWords(this.titolo)) || (this.checkIfThereAreItalianBadWords(this.descrizione) || this.checkIfThereAreEnglishBadWords(this.descrizione))) {
-      const toast = document.createElement('ion-toast');
-
-      toast.message = 'Hai inserito una parola scorretta!';
-      toast.duration = 2000;
-      toast.position = "top";
-      toast.style.fontSize = '20px';
-      toast.color = 'danger';
-      toast.style.textAlign = 'center';
-
-      document.body.appendChild(toast);
-      return toast.present();
+    if ((this.checkIfThereAreItalianBadWords(this.titolo) || this.checkIfThereAreEnglishBadWords(this.titolo))
+      || (this.checkIfThereAreItalianBadWords(this.descrizione) || this.checkIfThereAreEnglishBadWords(this.descrizione))) {
+      this.showMessage("Hai inserito una parola scorretta!", "danger");
     } else if (this.titolo.length < 1 || this.titolo.length > 150 || !(this.titolo.match(/[a-zA-Z0-9_]+/))) {
-
-      const toast = document.createElement('ion-toast');
-      toast.message = 'Devi inserire un titolo valido!';
-      toast.duration = 2000;
-      toast.position = "top";
-      toast.style.fontSize = '20px';
-      toast.color = 'danger';
-      toast.style.textAlign = 'center';
-      document.body.appendChild(toast);
-      return toast.present();
-
+      this.showMessage("Devi inserire un titolo valido!", "danger");
     } else if (this.descrizione.length > 1000) {
-
-      const toast = document.createElement('ion-toast');
-      toast.message = 'Descrizione troppo lunga!';
-      toast.duration = 2000;
-      toast.position = "top";
-      toast.style.fontSize = '20px';
-      toast.color = 'danger';
-      toast.style.textAlign = 'center';
-      document.body.appendChild(toast);
-      return toast.present();
-
+      this.showMessage("Devi inserire una descrizione valida!", "danger");
     } else if (this.categoriaScelta === undefined) {
-
-      const toast = document.createElement('ion-toast');
-      toast.message = 'Devi selezionare una categoria!';
-      toast.duration = 2000;
-      toast.position = "top";
-      toast.style.fontSize = '20px';
-      toast.color = 'danger';
-      toast.style.textAlign = 'center';
-      document.body.appendChild(toast);
-      return toast.present();
-
+      this.showMessage("Devi selezionare una categoria!", "danger");
     } else if (this.timerToPass === undefined) {
-
-      const toast = document.createElement('ion-toast');
-      toast.message = 'Devi impostare un timer!';
-      toast.duration = 2000;
-      toast.position = "top";
-      toast.style.fontSize = '20px';
-      toast.color = 'danger';
-      toast.style.textAlign = 'center';
-      document.body.appendChild(toast);
-      return toast.present();
-
+      this.showMessage("Devi impostare un timer!", "danger");
     } else {
-
-      const alert = await this.alertController.create({
-        header: 'Confermi la domanda?',
-        buttons: [
-          {
-            text: 'No',
-            role: 'cancel',
-            cssClass: 'secondary',
-            handler: (blah) => {
-              console.log('Confirm Cancel');
-            }
-          }, {
-            text: 'Si',
-            handler: () => {
-              console.log('Confirm Okay');
-              this.showOKToast();
-              this.postInvio();
-              this.dataService.setRefreshIndex(true);
-              this.goHome();
-            }
-          }
-        ]
-      });
-      await alert.present();
+      this.confirmAlert();
     }
-  }
-
- checkIfThereAreItalianBadWords(string: string): boolean {
-
-    let list = require('italian-badwords-list');
-
-    let array = list.array
-
-    console.log(array);
-
-    let stringArray = [];
-    let stringPassed = string.split(' ');
-    stringArray = stringArray.concat(stringPassed);
-
-    console.log(stringArray);
-
-    var check;
-
-    stringArray.forEach(element => {
-      if (array.includes(element))
-        check = true;
-    });
-
-    console.log(check);
-
-    return check;
 
   }
 
-  checkIfThereAreEnglishBadWords(string: string): boolean {
-
-    var Filter = require('bad-words'),
-      filter = new Filter();
-
-    return filter.isProfane(string)
-
-  }
- 
-
-  //CATEGORIA PICKER
 
   async showCategoriaPicker() {
 
@@ -222,38 +92,31 @@ export class InserisciDomandaPage implements OnInit {
         {
           text: 'Ok',
           handler: (value: any) => {
-            console.log(value);
-
-            this.categoriaView = value['ValoreCategoriaSettata'].text; //setto timerPopUp al valore inserito nel popUp una volta premuto ok così viene visualizzato
+            this.categoriaView = value['ValoreCategoriaSettata'].text;
             this.categoriaScelta = this.categoriaView;
             this.cod_categoria = value['ValoreCategoriaSettata'].value;
             this.codCategoriaScelta = this.cod_categoria;
-            console.log('Codice catgoria settata: ', this.codCategoriaScelta);
-
           }
         }
       ],
       columns: [{
-        name: 'ValoreCategoriaSettata', //nome intestazione json dato 
+        name: 'ValoreCategoriaSettata',
         options: this.getCategorieOptions()
       }]
     };
-
     let picker = await this.pickerController.create(options);
     picker.present()
-  }
 
+  }
   getCategorieOptions() {
+
     let options = [];
     this.categoriaSettings.forEach(x => {
       options.push({ text: x.titolo, value: x.codice_categoria });
     });
     return options;
+
   }
-
-
-  //TIMER PICKER
-
   async showTimerPicker() {
 
     let options: PickerOptions = {
@@ -265,34 +128,31 @@ export class InserisciDomandaPage implements OnInit {
         {
           text: 'Ok',
           handler: (value: any) => {
-            console.log(value);
-
             this.timerView = value['ValoreTimerSettato'].value;
             this.mappingTimerValueToPass(value['ValoreTimerSettato'].value);
-            console.log('Timer settato: ', this.timerToPass);
           }
         }
       ],
       columns: [{
-        name: 'ValoreTimerSettato', //nome intestazione json dato 
+        name: 'ValoreTimerSettato',
         options: this.getColumnOptions()
       }]
     };
-
     let picker = await this.pickerController.create(options);
     picker.present()
-  }
 
+  }
   getColumnOptions() {
+
     let options = [];
     this.timerSettings.forEach(x => {
       options.push({ text: x, value: x });
     });
     return options;
-  }
 
+  }
   mappingTimerValueToPass(valueToMapp) {
-    //converto valore timer da passare nel formato giusto per il db
+
     switch (valueToMapp) {
       case (this.timerSettings['0']):
         this.timerToPass = "00:05";
@@ -322,57 +182,112 @@ export class InserisciDomandaPage implements OnInit {
         this.timerToPass = "72:00";
         break;
     }
+
   }
 
 
-  //ROUTING
+  async sendData() {
 
-  goToCategoria() {
-    this.router.navigate(['proponi-categoria']);
-  }
+    this.apiService.inserisciDomanda(this.timerToPass, this.titolo, this.descrizione, this.cod_utente, this.codCategoriaScelta).then(
+      (result) => { },
+      (rej) => { }
+    );
 
-  goHome() {
-    //visualizza frame caricamento
-    const loading = document.createElement('ion-loading');
-    loading.cssClass = 'loading';
-    loading.spinner = 'crescent';
-    loading.duration = 3500;
-    document.body.appendChild(loading);
-    loading.present();
-
-    this.router.navigate(['home']);
-  }
-
-  goBack() {
-    //visualizza frame caricamento
-    const loading = document.createElement('ion-loading');
-    loading.cssClass = 'loading';
-    loading.spinner = 'crescent';
-    loading.duration = 3500;
-    document.body.appendChild(loading);
-    loading.present();
-    
-    this.navCtrl.back();
-  }
-
-  openMenu(){
-    this.menuCrtl.open()
-    console.log(this.menuCrtl.swipeGesture.length);
   }
 
 
-  //SUCCESS TOAST
+  checkIfThereAreItalianBadWords(string: string): boolean {
 
-  async showOKToast() {
+    let list = require('italian-badwords-list');
+    let array = list.array
+    let stringArray = [];
+    let stringPassed = string.split(' ');
+    stringArray = stringArray.concat(stringPassed);
+    var check;
+
+    stringArray.forEach(element => {
+      if (array.includes(element))
+        check = true;
+    });
+
+    return check;
+
+  }
+  checkIfThereAreEnglishBadWords(string: string): boolean {
+
+    var Filter = require('bad-words'),
+      filter = new Filter();
+
+    return filter.isProfane(string)
+
+  }
+
+
+  showMessage(message: string, color: string) {
+
     const toast = document.createElement('ion-toast');
-    toast.message = 'Inserimento avvenuto con successo!';
+    toast.message = message;
     toast.duration = 2000;
     toast.position = "top";
-    toast.style.fontSize = '20px';
-    toast.color = 'success';
-    toast.style.textAlign = 'center';
+    toast.color = color;
     document.body.appendChild(toast);
     return toast.present();
+
+  }
+  async confirmAlert() {
+
+    const alert = await this.alertController.create({
+      header: 'Confermi la domanda?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+          }
+        }, {
+          text: 'Si',
+          handler: () => {
+            this.showMessage("Inserimento avvenuto con successo!", "success");
+            this.sendData();
+            this.dataService.setRefreshIndex(true);
+            this.goHome();
+          }
+        }
+      ]
+    });
+    await alert.present();
+
+  }
+
+
+  goProponiCategoria() {
+    this.router.navigate(['proponi-categoria']);
+  }
+  goHome() {
+
+    const loading = document.createElement('ion-loading');
+    loading.cssClass = 'loading';
+    loading.spinner = 'crescent';
+    loading.duration = 3500;
+    document.body.appendChild(loading);
+    loading.present();
+    this.router.navigate(['home']);
+
+  }
+  goBack() {
+
+    const loading = document.createElement('ion-loading');
+    loading.cssClass = 'loading';
+    loading.spinner = 'crescent';
+    loading.duration = 3500;
+    document.body.appendChild(loading);
+    loading.present();
+    this.navCtrl.back();
+
+  }
+  openMenu() {
+    this.menuCrtl.open()
+    console.log(this.menuCrtl.swipeGesture.length);
   }
 
 }
